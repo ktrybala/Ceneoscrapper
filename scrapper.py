838d997cead8781d03b0a0716261e89bd8d9b8b0
Kacper Trybała <ktrybala@MacBook-ktrybala.local>
2022-04-08 11:08:36 +0200

    2022-04-08 KT

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,23 @@
+from termios import OPOST
 import requests
+from bs4 import BeautifulSoup
 
-url = "https://www.ceneo.pl/91714422#tab=reviews"
+url = "https://www.ceneo.pl/63490289#tab=reviews"
 response = requests.get(url)
-print(response.status_code)
+
+page = BeautifulSoup(response.text, 'html.parser')
+
+opinions = page.select("div.js_product-review")
+opinion = opinions.pop(0)
+opinion_id = opinion["data-entry-id"]
+author = opinion.select_one('span.user-post__author-name').get_text().strip()
+
+recomendations = opinion.select_one('span.user-post__author-recomendation > em').get_text()
+
+star_amount = opinion.select_one('span.user-post__score-count').get_text()
+
+opinion_text = opinion.select_one('div.user-post__text').get_text()
+
+print(opinion_text)
+
+
