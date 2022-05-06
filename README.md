7aea65737687da81df7921e487d9d7aee014d41a
Kacper Trybała <ktrybala@MacBook-ktrybala.local>
2022-05-06 11:12:10 +0200

    2022-05-06KT

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# CeneoScrapper
+# Scrapper
 
 ##Struktura opinii w serwisie Ceneo [Ceneo.pl](https://www.ceneo.pl/)
 
 |Składowa|Selektor|Nazwa zmiennej|Typ zmiennej|
 |--------|--------|--------------|------------|
-|opinia|div.js_product-review|user-post|obj|
-|indentyfikator opinii|div.js_product-review["data-entry-id"\]|data_entry_id|int|
-|autor opinii|span.user-post__author-name||||
-|rekomendacja|span.user-post__author-recomendation > em|||
-|liczba gwiazdek|span.user-post__score_count|user-post__score_count|string|
-|treść opinii|div.user-post__text|user-post__text|string|
-|lista zalet|div.review-feature__title--positives ~ div.review-feature__item|review-feature__col|obj|
-|lista wad|div.review-feature__title--negatives ~ div.review-feature__item|||
-|dla ilu osób przydatna|buttton.vote-yes > span|votes-yes-id|int|
-|dla ilu osób nieprzydatna|buttton.vote-no > span|votes-no-id|int|
-|data wystawienie opinii|span.user-post__published > time:nth-child(1)["datetime"]|user-post__published|list|
-|data zakupu|span.user-post__published > time:nth-child(2)["datetime"]|user-post__published|list|
+|opinia|div.js_product-review|opinion|bs4.element.Tag|
+|indentyfikator opinii|div.js_product-review["data-entry-id"\]|opinion_id|str|
+|autor opinii|span.user-post__author-name|author|str|
+|rekomendacja|span.user-post__author-recomendation > em|recomendation|str|
+|liczba gwiazdek|span.user-post__score-count|score|str|
+|treść opinii|div.user-post__text|content|str|
+|lista zalet|div.review-feature__title--positives ~ div.review-feature__item|pros|list|
+|lista wad|div.review-feature__title--negatives ~ div.review-feature__item|cons|list|
+|dla ilu osób przydatna|buttton.vote-yes > span|usefull|str|
+|dla ilu osób nieprzydatna|buttton.vote-no > span|useless|str|
+|data wystawienie opinii|span.user-post__published > time:nth-child(1)["datetime"]|published|str|
+|data zakupu|span.user-post__published > time:nth-child(2)["datetime"]|bought|str| 
--- scrapper.py
+++ scrapper.py
@@ -1,20 +1,47 @@
 import requests
+import json
 from bs4 import BeautifulSoup
-response = requests.get("https://www.ceneo.pl/63490289#tab=reviews")
-page = BeautifulSoup(response.text, 'html.parser')
-
-opinions = page.select("div.js_product-review")
-opinion = opinions.pop(3)
-opinion_id = opinion["data-entry-id"]
-author = opinion.select_one("span.user-post__author-name").get_text().strip()
-recomendation = opinion.select_one("span.user-post__author-recomendation > em").get_text().strip()
-score = opinion.select_one("span.user-post__score-count").get_text().strip()
-content = opinion.select_one("div.user-post__text").get_text()
-pros = [item.get_text() for item in opinion.select("div.review-feature__title--positives ~ div.review-feature__item")]
-cons = [item.get_text() for item in opinion.select("div.review-feature__title--negatives ~ div.review-feature__item")]
-usefull = opinion.select_one("button.vote-yes > span").get_text().strip()
-useless = opinion.select_one("button.vote-no > span").get_text().strip()
-publish_date = opinion.select_one("span.user-post__published > time:nth-child(1)")["datetime"]
-purchase_date = opinion.select_one("span.user-post__published > time:nth-child(2)")["datetime"]
-
-
+item_id = input("Insert item id:\n")
+url = f"https://www.ceneo.pl/{item_id}#tab=reviews"
+all_opinions = []
+while(url):
+    response = requests.get(url)
+    page = BeautifulSoup(response.text, 'html.parser')
+    opinions = page.select("div.js_product-review")
+    for opinion in opinions:
+        opinion_id = opinion["data-entry-id"]
+        author = opinion.select_one("span.user-post__author-name").get_text().strip()
+        try:
+            recomendation = opinion.select_one("span.user-post__author-recomendation > em").get_text().strip()
+        except AttributeError:
+            recomendation = None
+        score = opinion.select_one("span.user-post__score-count").get_text().strip()
+        content = opinion.select_one("div.user-post__text").get_text()
+        pros = [item.get_text() for item in opinion.select("div.review-feature__title--positives ~ div.review-feature__item")]
+        cons = [item.get_text() for item in opinion.select("div.review-feature__title--negatives ~ div.review-feature__item")]
+        usefull = opinion.select_one("button.vote-yes > span").get_text().strip()
+        useless = opinion.select_one("button.vote-no > span").get_text().strip()
+        publish_date = opinion.select_one("span.user-post__published > time:nth-child(1)")["datetime"]
+        try:
+            purchase_date = opinion.select_one("span.user-post__published > time:nth-child(2)")["datetime"]
+        except TypeError:
+            purchase_date = None
+        single_opinion = {
+            "opinion_id": opinion_id,
+            "author": author,
+            "recomendation": recomendation,
+            "score": score,
+            "pros": pros,
+            "cons": cons,
+            "usefull": usefull,
+            "useless": useless,
+            "publish_date": publish_date,
+            "purchase_date": purchase_date
+        }
+        all_opinions.append(single_opinion)
+    try:
+        url = f"https://www.ceneo.pl/{item_id}" + page.select_one("a.pagination__next")["href"]
+    except TypeError:
+        url = None
+with open(f"opinions/{item_id}.json", 'w', encoding="UTF-8") as jf:
+    json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
